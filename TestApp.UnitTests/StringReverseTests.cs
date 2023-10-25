9415fcf417d8a13b315864ab49f22afc4b637b9b
Stefan Ivanov <135533109+Stef40fit@users.noreply.github.com>
2023-10-25 14:13:36 +0300

    Add project files.

---
+++ TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        string input = "";
+
+        // Act
+        
+        string result = StringReverse.Reverse(input);
+        // Assert
+        Assert.AreEqual("", result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        string input = "a";
+
+        // Act
+
+        string result = StringReverse.Reverse(input);
+        // Assert
+        Assert.AreEqual("a", result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        string input = "abc";
+
+        // Act
+
+        string result = StringReverse.Reverse(input);
+        // Assert
+        Assert.AreEqual("cba", result);
+    }
+}
